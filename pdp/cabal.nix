7d4e5784bd57434bfe5965e6491db9e3854abcb2
Renzo Carbonara <gnuk0001@gmail.com>
2025-11-13 20:03:46 +0000

    nix

--- pdp/cabal.nix
+++ pdp/cabal.nix
@@ -1,15 +1,9 @@
-{ mkDerivation, aeson, base, binary, cassava, exceptions
-, kind-integer, kind-rational, lib, scientific, singletons, tagged
-, time
-}:
+{ mkDerivation, base, lib, scientific, singletons }:
 mkDerivation {
   pname = "pdp";
   version = "0.1.0";
   src = ./.;
-  libraryHaskellDepends = [
-    aeson base binary cassava exceptions kind-integer kind-rational
-    scientific singletons tagged time
-  ];
+  libraryHaskellDepends = [ base scientific singletons ];
   testHaskellDepends = [ base ];
   homepage = "https://github.com/k0001/hs-pdp";
   description = "Phantoms of Departed Proofs. Alternative API for the Ghost of Departed Proofs ideas";
